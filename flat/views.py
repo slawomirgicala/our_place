18224f39f0b5bac14540ed669d3d2e60b9d34a48
slawomir <sgicala98@gmail.com>
2019-06-11 20:08:06 +0200

    enter flat fixed

--- flat/views.py
+++ flat/views.py
@@ -77,17 +77,23 @@ def enter_flat(request):
                 messages.error(request, f'Wrong password!')
                 return redirect('enter-flat')  # strona glowna mieszkania
             else:
-                profile = request.user.profile
-                profile.flats.add(flat)
-                profile.active_flat = flat
-                profile.save()
-                chores = flat.chore_set
-                for chore in chores:
-                    counters = chore.chorecounter_set
-                    min_number = counters.aggregate(Min('number'))
-                    ChoreCounter(chore=chore, user=request.user, number=min_number).save()
-                messages.success(request, f'You entered a flat!')
-                return redirect('flat-home')  # strona glowna mieszkania
+                if flat in request.user.profile.flats.all():
+                    profile = request.user.profile
+                    profile.active_flat = flat
+                    profile.save()
+                    return redirect('flat-home')
+                else:
+                    profile = request.user.profile
+                    profile.flats.add(flat)
+                    profile.active_flat = flat
+                    profile.save()
+                    chores = flat.chore_set.all()
+                    for chore in chores:
+                        counters = chore.chorecounter_set
+                        min_number = counters.aggregate(Min('number'))['number__min']
+                        ChoreCounter(chore=chore, user=request.user, number=min_number).save()
+                    messages.success(request, f'You entered a flat!')
+                    return redirect('flat-home')  # strona glowna mieszkania
     else:
         form = FlatCreationForm()
     return render(request, 'flat/enter_flat.html', {'form': form})
@@ -95,25 +101,28 @@ def enter_flat(request):
 
 @login_required
 def new_chore(request):
-    if request.method == 'POST':
-        form = ChoreCreationForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            flat = request.user.profile.active_flat
-            period = form.cleaned_data['period']
-            chore = Chore(name=name, flat=flat, period=period, last_made=timezone.now())
-            chore.save()
-            profiles = request.user.profile.active_flat.profiles
-            for profile in profiles.iterator():
-                ChoreCounter(chore=chore, user=profile.user).save()
-            lucky = choice(profiles.all())
-            SpecificChore(user=lucky.user, flat=flat, start=timezone.now(),
-                          end=timezone.now()+timedelta(days=period), name=name).save()
-            messages.success(request, f'Added new chore :c')
-            return redirect('chores-list')
+    if request.user.profile.active_flat is not None:
+        if request.method == 'POST':
+            form = ChoreCreationForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                flat = request.user.profile.active_flat
+                period = form.cleaned_data['period']
+                chore = Chore(name=name, flat=flat, period=period, last_made=timezone.now())
+                chore.save()
+                profiles = request.user.profile.active_flat.profiles
+                for profile in profiles.iterator():
+                    ChoreCounter(chore=chore, user=profile.user).save()
+                lucky = choice(profiles.all())
+                SpecificChore(user=lucky.user, flat=flat, start=timezone.now(),
+                              end=timezone.now()+timedelta(days=period), name=name).save()
+                messages.success(request, f'Added new chore :c')
+                return redirect('chores-list')
+        else:
+            form = ChoreCreationForm()
+        return render(request, 'flat/new_chore.html', {'form': form})
     else:
-        form = ChoreCreationForm()
-    return render(request, 'flat/new_chore.html', {'form': form})
+        return redirect('new-flat')
 
 
 @login_required
@@ -201,11 +210,25 @@ def your_chores(request):
     if request.user.profile.active_flat is None:
         return redirect('new-flat')
     else:
-
-        chores = SpecificChore.objects.filter(flat=request.user.profile.active_flat, user=request.user,
-                                              completed=False).order_by('start')
+        flat_chores = request.user.profile.active_flat.chore_set.all()
+        for chore in flat_chores:
+            if timezone.now() - chore.last_made > timedelta(days=chore.period):
+                print('hey')
+                min_number = chore.chorecounter_set.aggregate(Min('number'))['number__min']
+                #print(min_number)
+                random_chore_counter = chore.chorecounter_set.all().filter(number=min_number)[0]
+                random_chore_counter.number += 1
+                random_chore_counter.save()
+                #user = (ChoreCounter.objects.filter(chore=chore).values_list('user')
+                #        .annotate(Min('number')).order_by('number')[0])
+                SpecificChore(user=random_chore_counter.user, flat=request.user.profile.active_flat, start=timezone.now(),
+                              end=timezone.now()+timedelta(days=chore.period), name=chore.name).save()
+                chore.last_made = timezone.now()
+                chore.save()
+        your_active_chores = SpecificChore.objects.filter(flat=request.user.profile.active_flat, user=request.user,
+                                                          completed=False).order_by('start')
         context = {
-            'chores': chores
+            'chores': your_active_chores
         }
         return render(request, 'flat/your_chores.html', context)
 
